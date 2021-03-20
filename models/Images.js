e6502ce2259adc6509a95dff4b1eb56add141984
Rajahni fields <rajahnifields@gmail.com>
2021-03-20 17:07:41 -0400

    added manifest

---
+++ models/Images.js
@@ -0,0 +1,13 @@
+// var mongoose = require("mongoose");
+// var Schema = mongoose.Schema;
+
+// var iconSchema = new Schema({
+//   image: { type: String, required: true },
+//   description: { type: String, required: true },
+//   rating: Number,
+//   date: { type: Date, default: Date.now },
+// });
+
+// var Image = mongoose.model("Image", iconSchema);
+
+// module.exports = Image;
